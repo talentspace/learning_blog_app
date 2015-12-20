0cb21b35bad917c4524b5d3ba2074617d784d46d
Ershad Kunnakkadan <ershad92@gmail.com>
2015-12-20 15:01:02 +0530

    Added update and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,51 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
   def index
     @posts = Post.all
+  end
+
+  def create
+    title = params[:post][:title]
+    body = params[:post][:body]
+
+    Post.create title: title, body: body
+
+    flash[:notice] = "Successfully created post"
+
+    redirect_to root_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+
+    title = params[:post][:title]
+    body = params[:post][:body]
+
+    post.title = title
+    post.body = body
+
+    post.save
+
+    flash[:notice] = "Updated  post"
+
+    redirect_to post_path(post)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+
+    flash[:notice] = "Deleted post"
+
+    redirect_to root_path
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get '/posts' => 'posts#index'
+  get '/posts/new' => 'posts#new', as: :new_post
   get '/posts/:id' => 'posts#show', as: :post
+  post '/posts' => 'posts#create'
+  get '/posts/:id/edit' => 'posts#edit'
+  patch '/posts/:id' => 'posts#update'
+  delete '/posts/:id' => 'posts#destroy', as: :destroy_post
 
   root 'posts#index'
 
